69808274125822be6a4c0f671fbcfc5fc5edd53b
Mayank Goyal <heromayank2@gmail.com>
2019-10-22 18:41:08 +0530

    First COMMIT

---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express')
+var mysql = require('mysql')
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+const BodyParser = require('body-parser')
+
+var app = express()
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true
+    })
+);
+const PORT = process.env.PORT || 1337;
+
+var connection = mysql.createConnection({
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "erp"
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Error in Connecting to Database ", err)
+    } else {
+        console.log("Connected to Database")
+    }
+})
+
+app.get('/', (req, res) => {
+    connection.query("SELECT * FROM student", (err, rows, fields) => {
+        return res.send(rows)
+    })
+})
+
+app.get('/login', (req, res) => {
+    return res.render('login')
+})
+
+app.post('/login', (req, res) => {
+    const { sid, password } = req.body
+    let query = "SELECT * FROM student"
+    connection.query(query, (err, rows, fields) => {
+        i = 0
+        for (; i < rows.length; i++) {
+            if (rows[i].ID == sid) {
+                break
+            }
+        }
+        if (rows[i].password == password) {
+            var user = rows[i]
+            return res.render("dashboard", { user })
+        }
+        return res.send(404)
+    })
+})
+
+app.get('/assignments', (req, res) => {
+    connection.query("SELECT * FROM assignment", (error, rows, fields) => {
+        return res.send(rows)
+    })
+})
+
+app.use("/", express.static(__dirname + '/assets/'));
+
+app.listen(PORT, console.log("Listening on PORT " + PORT));
